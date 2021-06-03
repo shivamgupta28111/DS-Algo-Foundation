9892a7ce2b4ace5d5a32c2ae8347a616004a0123
Tube light Coder <70341160+shivamgupta28111@users.noreply.github.com>
2021-06-03 21:25:35 +0530

    Create CoinChangePermutation_DP.java

---
+++ CoinChangePermutation_DP.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scn= new Scanner(System.in);
+        int n= scn.nextInt();
+        
+        int[] coins=new int[n];
+        
+        for(int i=0;i<coins.length;i++){
+            
+            coins[i]=scn.nextInt();
+        }
+
+        
+        int tar = scn.nextInt();
+        
+        int[] dp=new int[tar+1];
+        dp[0]=1;
+        
+        for(int amount=1; amount<=tar; amount++){
+            for(int coin:coins){
+               
+                if(coin<=amount){
+                    
+                    if(coin<=amount){
+                        
+                        int remainingAmt=amount-coin;
+                        
+                        dp[amount] +=dp[remainingAmt];
+                    }
+                    
+                }
+            } 
+            
+            
+        }
+        
+        System.out.println(dp[tar]);
+        
+    }
+}
