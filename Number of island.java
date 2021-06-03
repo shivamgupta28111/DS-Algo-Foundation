2eba20502cd9d0a7f9a0a1679e69bb33cf0d34d2
Tube light Coder <70341160+shivamgupta28111@users.noreply.github.com>
2021-06-04 03:21:50 +0530

    Create Number of island.java

---
+++ Number of island.java
@@ -0,0 +1,56 @@
+//                               Using DFS and Backtracking Recusrion like flood fill
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int m = Integer.parseInt(br.readLine());
+      int n = Integer.parseInt(br.readLine());
+      int[][] arr = new int[m][n];
+
+      for (int i = 0; i < arr.length; i++) {
+         String parts = br.readLine();
+         for (int j = 0; j < arr[0].length; j++) {
+            arr[i][j] = Integer.parseInt(parts.split(" ")[j]);
+         }
+      }
+
+      // write your code here
+      
+      boolean[][] vis = new boolean[m][n];
+      int count =0;
+      for(int i=0;i<m;i++){
+          for(int j=0;j<n;j++){
+              
+              if(arr[i][j]==0 && vis[i][j] != true){
+                  dfs(arr,i,j,vis);
+                   count +=1;
+              }
+              
+          }
+          
+        
+      }
+       System.out.println(count); 
+   }
+   
+   public static void dfs(int[][] arr, int i,int j,boolean[][] vis){
+       
+       if(i<0|| i==arr.length|| j<0||j==arr[0].length||arr[i][j]!=0||vis[i][j]==true){
+           return;
+       }
+       
+       vis[i][j]=true;
+       
+       dfs(arr,i-1,j,vis);
+       dfs(arr,i+1,j,vis);
+       dfs(arr,i,j-1,vis);
+       dfs(arr,i,j+1,vis);
+       
+       
+   }
+
+}
